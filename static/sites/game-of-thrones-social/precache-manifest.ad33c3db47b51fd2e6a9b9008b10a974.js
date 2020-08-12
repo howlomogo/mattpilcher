5a822cb083a8b4ceebc2322ed4c1f1e964c3c815
Matt Pilcher <mattpilcher87@gmail.com>
2020-08-12 13:51:18 +0100

    update homepage for build

---
+++ static/sites/game-of-thrones-social/precache-manifest.ad33c3db47b51fd2e6a9b9008b10a974.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6398582ab58bd19018f565ee7b2d7cac",
+    "url": "/sites/game-of-thrones-social/index.html"
+  },
+  {
+    "revision": "6a20f72482ba7e0c4d13",
+    "url": "/sites/game-of-thrones-social/static/css/main.aa356034.chunk.css"
+  },
+  {
+    "revision": "daed879f98344de7514a",
+    "url": "/sites/game-of-thrones-social/static/js/2.4dfa0ebf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/sites/game-of-thrones-social/static/js/2.4dfa0ebf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a20f72482ba7e0c4d13",
+    "url": "/sites/game-of-thrones-social/static/js/main.442da186.chunk.js"
+  },
+  {
+    "revision": "944cd586b5cf2b659cf9",
+    "url": "/sites/game-of-thrones-social/static/js/runtime-main.4a038931.js"
+  }
+]);
--- static/sites/game-of-thrones-social/service-worker.js
+++ static/sites/game-of-thrones-social/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3e2198d131e32295129a002f6f58e398.js"
+  "/sites/game-of-thrones-social/precache-manifest.ad33c3db47b51fd2e6a9b9008b10a974.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sites/game-of-thrones-social/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
