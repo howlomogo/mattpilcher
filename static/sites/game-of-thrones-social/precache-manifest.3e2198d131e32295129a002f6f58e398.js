f4f682f97fbb4fdd80d5f5c3c0bcf743ce27865f
Matt Pilcher <mattpilcher87@gmail.com>
2020-08-12 13:38:38 +0100

    adding got site

---
+++ static/sites/game-of-thrones-social/precache-manifest.3e2198d131e32295129a002f6f58e398.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bdaf663025f1769bfa9142f48215b0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a20f72482ba7e0c4d13",
+    "url": "/static/css/main.aa356034.chunk.css"
+  },
+  {
+    "revision": "daed879f98344de7514a",
+    "url": "/static/js/2.4dfa0ebf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4dfa0ebf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a20f72482ba7e0c4d13",
+    "url": "/static/js/main.442da186.chunk.js"
+  },
+  {
+    "revision": "6cad4a341686d2adfb75",
+    "url": "/static/js/runtime-main.cfc120ef.js"
+  }
+]);
