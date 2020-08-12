e0bfd75f0a39cccc71d26af4e9dd88536918cb61
Matt Pilcher <mattpilcher87@gmail.com>
2020-08-12 14:29:07 +0100

    Fix for got site

---
+++ static/sites/game-of-thrones-social/precache-manifest.095ccca8d515f14e1363f83090db0454.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0ffcf8859fc4419e7246306bd05fa05",
+    "url": "/sites/game-of-thrones-social/index.html"
+  },
+  {
+    "revision": "e3868a6d32c5067ff415",
+    "url": "/sites/game-of-thrones-social/static/css/main.aa356034.chunk.css"
+  },
+  {
+    "revision": "daed879f98344de7514a",
+    "url": "/sites/game-of-thrones-social/static/js/2.4dfa0ebf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/sites/game-of-thrones-social/static/js/2.4dfa0ebf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3868a6d32c5067ff415",
+    "url": "/sites/game-of-thrones-social/static/js/main.85223f9b.chunk.js"
+  },
+  {
+    "revision": "944cd586b5cf2b659cf9",
+    "url": "/sites/game-of-thrones-social/static/js/runtime-main.4a038931.js"
+  }
+]);
